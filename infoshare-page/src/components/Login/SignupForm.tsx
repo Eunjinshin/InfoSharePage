a713d1160010da5c3613c322af0da901a1f7c497
Eunjinshin <58281884+Eunjinshin@users.noreply.github.com>
2026-03-12 15:27:19 +0900

    Rename setUsername to _setUsername in SignupForm

--- infoshare-page/src/components/Login/SignupForm.tsx
+++ infoshare-page/src/components/Login/SignupForm.tsx
@@ -11,7 +11,7 @@ export const SignupForm: React.FC = () => {
 
     // 폼 상태 관리
     const [name, setName] = useState('');
-    const [username, setUsername] = useState('');
+    const [username, _setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
